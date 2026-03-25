0603cca4d90dcdade90f04d487ecc6e55ad9dc1e
tetarwal005 <tetarwal005@gmail.com>
2026-03-25 10:51:31 +0530

    Create ReAct agent and connect calculator tool

--- backend/app/agent/agent.py
+++ backend/app/agent/agent.py
@@ -1,17 +1,34 @@
 from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+
 from app.config import settings
+from app.tools.calculator import calculator
+
 
 def get_llm():
-    llm = ChatGroq(
+    return ChatGroq(
         model="llama-3.3-70b-versatile",
         api_key=settings.GROQ_API_KEY,
         temperature=0
     )
-    return llm
 
 
-if __name__ == "__main__":
+def get_agent():
     llm = get_llm()
     
-    response = llm.invoke("Explain AI in one line")
-    print(response.content)
+    tools = [calculator]   # 👈 attach tool here
+    
+    agent = create_agent(llm, tools)
+    
+    return agent
+
+
+# test agent
+if __name__ == "__main__":
+    agent = get_agent()
+
+    response = agent.invoke({
+        "messages": [("user", "What is 5 * 10 + 3?")]
+    })
+
+    print(response)
